7e15c3f71986ac26db8f06488dff7f4d3daf2f37
itsAnian <anian.seidl@gmail.com>
2025-03-25 19:07:34 +0100

    Add collision
    works great for small amount of balls
    if balls get pushed by another ball into other balls it gets buggy

--- bouncing_balls/ball.py
+++ bouncing_balls/ball.py
@@ -25,11 +25,38 @@ class Ball:
         self.speed_x *= self.friction
         self.speed_y *= self.friction
 
-        if self.x - self.radius < 0 or self.x + self.radius > width:
-            self.speed_x *= -1
-        if self.y + self.radius > height:
-            self.y = height - self.radius
-            if abs(self.speed_y) >= 0.5:
-                self.speed_y *= -1
+        self.check_border(width, height)
+
+        for ball in balls:
+            self.check_collision(ball, width, height)
+
+    def check_border(ball, width, height):
+        if ball.x - ball.radius < 0 or ball.x + ball.radius > width:
+            ball.speed_x *= -1
+        if ball.y + ball.radius > height:
+            ball.y = height - ball.radius
+            if abs(ball.speed_y) >= 0.5:
+                ball.speed_y *= -1
             else:
-                self.speed_y = 0
+                ball.speed_y = 0
+
+    def check_collision(self, other, width, height):
+        if other != self:
+            dx = other.x - self.x
+            dy = other.y - self.y
+            distance = math.sqrt(dx**2 + dy**2)
+
+            if distance < self.radius + other.radius:
+                nx = dx / distance
+                ny = dy / distance
+
+                self.speed_x, other.speed_x = other.speed_x, self.speed_x
+                self.speed_y, other.speed_y = other.speed_y, self.speed_y
+
+                overlap = self.radius + other.radius - distance
+                self.x -= nx * (overlap / 2)
+                self.y -= ny * (overlap / 2)
+                other.x += nx * (overlap / 2)
+                other.y += ny * (overlap / 2)
+
+                other.check_border(width, height)
